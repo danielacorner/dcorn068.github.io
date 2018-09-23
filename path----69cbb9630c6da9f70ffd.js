f4df34a6c2e7c8543ca86c906dfa8e09f99978d0
dcorn068 <dcorn068@users.noreply.github.com>
2018-09-23 19:45:25 -0400

    Updates

--- path----69cbb9630c6da9f70ffd.js
+++ path----69cbb9630c6da9f70ffd.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{274:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Daniel Corner"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{270:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Daniel Corner"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----69cbb9630c6da9f70ffd.js.map
